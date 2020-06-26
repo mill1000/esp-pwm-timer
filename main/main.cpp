b8a5d925967bd06ae078a335813f158f6b188d47
Tucker Kern <tuckkern@gmail.com>
2020-06-25 19:32:03 -0600

    Use an EventGroup to signal NTP updates to the main loop

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include "esp_err.h"
 #include "nvs_flash.h"
 #include "esp_sntp.h"
@@ -10,6 +11,14 @@
 
 #include "wifi.h"
 #define TAG "Main"
+
+typedef enum
+{
+  MAIN_EVENT_SYSTEM_TIME_UPDATED = 1 << 0,
+  MAIN_EVENT_ALL                 = 0x00FFFFFF, // 24 bits max
+} MAIN_EVENT;
+
+static EventGroupHandle_t mainEventGroup;
 
 static void sntpInit(const std::string& timezone)
 {
@@ -31,6 +40,8 @@ static void sntpInit(const std::string& timezone)
     struct tm* timeinfo = localtime(&tv->tv_sec);
     ESP_LOGI(TAG, "System time set to %s", asctime(timeinfo));
 
+    if (mainEventGroup != NULL)
+      xEventGroupSetBits(mainEventGroup, MAIN_EVENT_SYSTEM_TIME_UPDATED);
   });
 
   sntp_init();
@@ -50,8 +61,23 @@ extern "C" void app_main()
   // Initalize WiFi and connect to configured network
   wifiInitStation();
 
+  // Create an event group to run the main loop from
+  mainEventGroup = xEventGroupCreate();
+  if (mainEventGroup == NULL)
+    ESP_LOGE(TAG, "Failed to create main event group.");
 
   // Configure NTP for MST/MDT
   sntpInit("MST7MDT,M3.2.0,M11.1.0");
 
+  // Wait for initial time sync
+  xEventGroupWaitBits(mainEventGroup, MAIN_EVENT_SYSTEM_TIME_UPDATED, pdTRUE, pdFALSE, portMAX_DELAY);
+
+  while (true)
+  {
+    EventBits_t events = xEventGroupWaitBits(mainEventGroup, MAIN_EVENT_ALL, pdTRUE, pdFALSE, portMAX_DELAY);
+
+    if (events & MAIN_EVENT_SYSTEM_TIME_UPDATED)
+    {
+    }
+  }
 }
