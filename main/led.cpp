24d7a409db9cdbd985f8e64034043373e6181ef8
Tucker Kern <tuckkern@gmail.com>
2020-06-27 22:12:07 -0600

    try configuring LED driver with structs

--- main/led.cpp
+++ main/led.cpp
@@ -8,35 +8,84 @@
 #include <cstring>
 
 #include "led.h"
+#include "schedule.h"
+#include "nvs_interface.h"
 
 #define TAG "LED"
 
-void ledInit(uint32_t frequency_Hz)
+void ledConfigureTimer(const timer_config_t& config)
 {
-  // Configure timer 0
+  if (config.id >= LEDC_TIMER_MAX)
+  {
+    ESP_LOGW(TAG, "Invalid timer ID: %d", config.id);
+    return;
+  }
+
   ledc_timer_config_t timer_config;
   memset(&timer_config, 0, sizeof(ledc_timer_config_t));
 
   timer_config.speed_mode = LED_MODE;
   timer_config.duty_resolution = LED_RESOLUTION;
-  timer_config.timer_num = LED_TIMER;
-  timer_config.freq_hz = frequency_Hz;
+  timer_config.timer_num = (ledc_timer_t) config.id;
+  timer_config.freq_hz = config.frequency_Hz;
   timer_config.clk_cfg = LEDC_AUTO_CLK;
 
-  ledc_timer_config(&timer_config);
+  ESP_LOGI(TAG, "Timer %d set to %d Hz", config.id, config.frequency_Hz);
 
-  // Configure channel0
+  ledc_timer_config(&timer_config);
+}
+
+void ledConfigureChannel(const channel_config_t& config)
+{
+  if (config.id >= LEDC_CHANNEL_MAX)
+  {
+    ESP_LOGW(TAG, "Invalid channel ID: %d", config.id);
+    return;
+  }
+
+  if (config.enabled == false)
+  {
+    ESP_LOGI(TAG, "Channel %d: disabled", config.id);
+    ledc_stop(LED_MODE, config.id, 0);
+    return;
+  }
+
+  if (config.gpio == GPIO_NUM_NC || config.gpio >= GPIO_NUM_MAX)
+  {
+    ESP_LOGW(TAG, "Invalid GPIO: %d", config.gpio);
+    return;
+  }
+
+  if (config.timer >= LEDC_TIMER_MAX)
+  {
+    ESP_LOGW(TAG, "Invalid timer: %d", config.gpio);
+    return;
+  }
+
   ledc_channel_config_t channel_config;
   memset(&channel_config, 0, sizeof(ledc_channel_config_t));
 
-  channel_config.channel    = LEDC_CHANNEL_0;
+  channel_config.channel    = config.id;
   channel_config.duty       = 0;
-  channel_config.gpio_num   = 18;
+  channel_config.gpio_num   = config.gpio;
   channel_config.speed_mode = LED_MODE;
   channel_config.hpoint     = 0;
-  channel_config.timer_sel  = LED_TIMER;
+  channel_config.timer_sel  = config.timer;
+
+  ESP_LOGI(TAG, "Channel %d: GPIO: %d Timer: %d Name: %s", config.id, config.gpio, config.timer, "");//config.name.c_str());
 
   ledc_channel_config(&channel_config);
+}
+
+void ledInit(uint32_t frequency_Hz)
+{
+  // Configure timer 0
+  timer_config_t timer = NVS::get_timer_config(0);
+  ledConfigureTimer(timer);
+
+  // Configure channel0
+  channel_config_t channel = NVS::get_channel_config(0);
+  ledConfigureChannel(channel);
 
   // Enable fade function
   ledc_fade_func_install(0);
